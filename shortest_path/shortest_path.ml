3e8120c3c0b8f71da1d15f1a6128982f5b81f0c6
Masahiro Masuda <masahi129@gmail.com>
2019-05-17 10:54:09 +0900

    remove metro stuff

--- shortest_path/shortest_path.ml
+++ shortest_path/shortest_path.ml
@@ -1,23 +1,8 @@
-open Metro
 open Core_kernel
 open Types
 
 module DistMap = Treemap.Make(String)
 module Heap = Core_kernel.Heap
-
-let lookup_name k name_list =
-  match List.find ~f:(fun {kanji; _} -> kanji = k) name_list with
-  | Some {romaji; _} -> romaji
-  | None -> assert false
-
-let get_edge_list name_list ekikan_list =
-  List.map ~f:(fun {kiten; shuten; kyori; _} ->
-      let src_name = lookup_name kiten name_list in
-      let dst_name = lookup_name shuten name_list in
-      (src_name, {dst = dst_name; weight = kyori})) ekikan_list
-
-let get_node_list name_list =
-  List.map ~f:(fun {romaji; _} -> romaji) name_list
 
 let build_graph edge_list =
   let update_edge_list graph (src, {dst; weight}) = Graph.add_edge graph src dst weight in
@@ -66,17 +51,9 @@ let run src dst edge_list =
   List.iter ~f:(fun n -> Printf.printf " %s -> " n) path;
   Printf.printf "%s\n" dst
 
-let run_tokyo_metro () =
-  let edge_list = get_edge_list global_ekimei_list global_ekikan_list in
-  let reverse_edge (src, {dst; weight}) = (dst, {dst = src; weight}) in
-  let (src, dst) = ("myogadani", "meguro") in
-  List.fold_right edge_list ~init:edge_list ~f:(fun edge acc -> (reverse_edge edge) :: acc)
-  |> run src dst
-
 let run_dimacs () =
   let (src, dst) = ("500", "9998") in
-  Dimacs.load_edge_list "USA-road-d.W.gr" |> run src dst
+  Dimacs.load_edge_list "USA-road-d.NY.gr" |> run src dst
 
 let () =
-  run_tokyo_metro ();
   run_dimacs ()
