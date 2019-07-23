d5f958b2f5faa2bb6d507190e0d43720cb61cd6f
Masahiro Masuda <masahi129@gmail.com>
2019-07-23 23:17:47 +0900

    add binomial and student t sampling

--- probprog/prob_monad.ml
+++ probprog/prob_monad.ml
@@ -87,5 +87,45 @@ let () =
     return (x1 + x2)
   in
   let open Owl in
-  let samples = Mat.init 1000 1 (fun _ -> float_of_int (sample_dist sum_dist)) in
+  let samples = Mat.init 1000 1 (fun _ -> sample_dist sum_dist |> float_of_int) in
   plot_hist "loaded_die_sum2.png" samples
+
+
+let () =
+  let binomial p n =
+    let ber = Primitive(Bernoulli(p)) in
+    let rec helper n acc =
+      if n = 0 then return acc
+      else
+        let* x = ber in
+        helper (n - 1) (acc + (Bool.to_int x))
+    in
+    helper n 0
+  in
+  let dist = binomial 0.3 100 in
+  let open Owl in
+  let samples = Mat.init 1000 1 (fun _ -> sample_dist dist |> float_of_int) in
+  plot_hist ~bin:30 "binomial_1000_0.3.png" samples
+
+
+let () =
+  let normal = Primitive(Gaussian(0.0, 1.0)) in
+  let chi2 k =
+    let rec helper n acc =
+      if n = 0 then return acc
+      else
+        let* x = normal in
+        helper (n - 1) (acc +. x *. x)
+    in
+    helper k 0.0
+  in
+  let student_t df =
+    let* z = normal in
+    let* v = chi2 df in
+    let rv = z *. sqrt (float_of_int df /. v) in
+    return rv
+  in
+  let open Owl in
+  let dist = student_t 10 in
+  let samples = Mat.init 1000 1 (fun _ -> sample_dist dist) in
+  plot_hist ~bin:50 "student_t_5.png" samples
