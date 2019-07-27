5a60357734418b7a8fa46cbd2d2b3f5b0b7c44c0
Masahiro Masuda <masahi129@gmail.com>
2019-07-27 18:58:17 +0900

    jit working

--- metaocaml/power_offshore.ml
+++ metaocaml/power_offshore.ml
@@ -44,7 +44,6 @@ let pprint_fun args typ exp =
   let body = pprint_exp exp in
   Printf.printf "){\n %s\n}\n" body
 
-
 let pprint_proc args cmd =
   Printf.printf "Proc\n"
 
@@ -66,19 +65,30 @@ let create_entry_block_alloca the_function var_name =
   in
   Llvm.build_alloca double_type var_name builder
 
-let gen_expr body =
-  Base.Hashtbl.find_exn named_values "x_1"
-(* function
-   * | Const (Const_float x) ->  Llvm.const_float double_type (Float.of_string x)
-   * (\* | LocalVar (x,_) -> pprint_vname x
-   *  * | Let({id; ty; bind; body; _}) ->
-   *  *   let rhs = pprint_exp bind in
-   *  *   let body = pprint_exp body in
-   *  *   Printf.sprintf "let %s:%s = %s in \n %s" (string_of_vname id) (string_of_typ ty) rhs body
-   *  * | FunCall (name,args) ->
-   *  *   let args_str = List.map pprint_exp args in
-   *  *   Printf.sprintf "app(%s, %s)" (string_of_vname name) (String.concat ", " args_str) *\)
-   * | _ ->  failwith "not yet implemented" *)
+let rec gen_expr the_function = function
+  | Const (Const_float x) ->
+    Llvm.const_float double_type (Float.of_string x)
+  | LocalVar (name,_) ->
+    begin match Base.Hashtbl.find named_values (string_of_vname name) with
+      | None -> failwith "Variable not found"
+      | Some(v) -> Llvm.build_load v (string_of_vname name) builder
+    end
+  | Let({id; ty; bind; body; _}) ->
+    let rhs = gen_expr the_function bind in
+    let alloca = create_entry_block_alloca the_function (string_of_vname id) in
+    Llvm.build_store rhs alloca builder |> ignore ;
+    Hashtbl.add_exn named_values ~key:(string_of_vname id) ~data:alloca;
+    gen_expr the_function body
+  | FunCall (name,args) ->
+    let open Base in
+    let args = List.map ~f:(gen_expr the_function) args in
+    begin match (string_of_vname name) with
+      | "*." ->
+        assert(List.length args = 2);
+        Llvm.build_fmul (List.nth_exn args 0) (List.nth_exn args 1) "multmp" builder
+      | _ -> failwith "not yet implemented"
+    end
+  | _ ->  failwith "not yet implemented"
 
 let gen_func args body fpm =
   let doubles = Array.create ~len:(List.length args) double_type in
@@ -97,24 +107,29 @@ let gen_func args body fpm =
       let alloca = create_entry_block_alloca the_function var_name in
       Llvm.build_store ai alloca builder |> ignore;
       Hashtbl.set named_values ~key:var_name ~data:alloca);
-  let return_val = gen_expr body in
+  let return_val = gen_expr the_function body in
   let _  = Llvm.build_ret return_val builder in
+  Llvm.dump_value the_function;
   let _  = Llvm.PassManager.run_function the_function fpm in
-  the_function
+  match Llvm_analysis.verify_function the_function with
+  | true -> the_function
+  | false -> assert false
 
 let gen_llvm fpm = function
  | Fun(args, typ, exp) -> gen_func args exp fpm
  | Proc(args, cmd) -> assert false
 
 let _ =
-  ignore(Llvm_executionengine.initialize ());
+  Llvm_executionengine.initialize () |> ignore;
   let ee = Llvm_executionengine.create llmodule in
   let fpm = Llvm.PassManager.create_function llmodule in
   Llvm_scalar_opts.add_memory_to_register_promotion fpm ;
-  let go n _ =
+  Llvm.PassManager.initialize fpm |> ignore;
+  let go n x =
     let power_staged = spowern n in
     let func = offshore (module DefaultConv) power_staged in
     pprint_offshore_ir func;
+    flush stdout;
     let llval = gen_llvm fpm func in
     Llvm.dump_value llval;
     Llvm_executionengine.add_module llmodule ee;
@@ -123,7 +138,7 @@ let _ =
         (Foreign.funptr Ctypes.(double @-> returning double))
         ee
     in
-    Stdlib.Printf.printf "Evaluated to %f\n" (fp 5.0) ;
+    Stdlib.Printf.printf "Evaluated to %.20f\n" (fp x) ;
     Llvm_executionengine.remove_module llmodule ee
   in
   go 100000 1.00001
