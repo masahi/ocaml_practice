9fad1452e69b7ccb636ff84060df7f3a3fe56578
Masahiro Masuda <masahi129@gmail.com>
2019-09-30 22:50:30 +0900

    add identifier

--- parser_combinator/main.ml
+++ parser_combinator/main.ml
@@ -1,3 +1,8 @@
+let string_of_chars chars =
+  let buf = Buffer.create 16 in
+  List.iter (Buffer.add_char buf) chars;
+  Buffer.contents buf
+
 module Parser = struct
   open Base
 
@@ -6,6 +11,19 @@ module Parser = struct
     fun input ->
       if String.equal expected (String.sub input ~pos:0 ~len) then Ok(String.subo input ~pos:len, ())
       else Error(input)
+
+  let identifier input =
+    let char_list = String.to_list input in
+    match char_list with
+    | [] -> Error(input)
+    | hd :: tl ->
+      if Char.is_alpha hd then
+        let consumed =
+          hd :: List.take_while tl ~f:(fun c -> Char.is_alphanum c || Char.equal c '-')
+        in
+        Ok(String.subo input ~pos:(List.length consumed), string_of_chars consumed)
+      else Error(input)
+
 end
 
 
@@ -15,3 +33,9 @@ let _ =
   assert (Ok("", ()) = parse_joe "Hello Joe!");
   assert (Ok(" Hello Robert!", ()) = parse_joe("Hello Joe! Hello Robert!"));
   assert (Error("Hello Mike!") = parse_joe("Hello Mike!"))
+
+let _ =
+  let open Parser in
+  assert (Ok("", "i-am-an-identifier") = identifier("i-am-an-identifier"));
+  assert (Ok(" entirely an identifier", "not") = identifier("not entirely an identifier"));
+  assert (Error("!not at all an identifier") = identifier("!not at all an identifier"))
