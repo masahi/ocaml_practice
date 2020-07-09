bcf71477bb79e35e3c3a503e6326e77d49451252
Masahiro Masuda <masahi129@gmail.com>
2020-07-09 13:02:27 +0900

    first staged fact and add bench

--- metaocaml/taha_part1.ml
+++ metaocaml/taha_part1.ml
@@ -1,3 +1,5 @@
+open Codelib
+
 type exp = Int of int | Var of string | App of string * exp
          | Add of exp * exp | Sub of exp * exp
          | Mul of exp * exp | Div of exp * exp | Ifz of exp * exp * exp
@@ -41,6 +43,36 @@ let rec peval1 p env fenv =
     let rec f x = eval1 e1 (ext env s2 x) (ext fenv s1 f)
     in peval1 (Program(tl, e)) env (ext fenv s1 f)
 
+let rec eval2 e env fenv =
+  match e with
+  | Int i -> .<i>.
+  | Var s -> env s
+  | App (s,e2) -> .<.~(fenv s) .~(eval2 e2 env fenv)>.
+  | Add (e1,e2) -> .<.~(eval2 e1 env fenv) + .~(eval2 e2 env fenv)>.
+  | Sub (e1,e2) -> .<.~(eval2 e1 env fenv) - .~(eval2 e2 env fenv)>.
+  | Mul (e1,e2) -> .<.~(eval2 e1 env fenv) * .~(eval2 e2 env fenv)>.
+  | Div (e1,e2) -> .<.~(eval2 e1 env fenv) / .~(eval2 e2 env fenv)>.
+  | Ifz (e1,e2,e3) ->
+    .<if .~(eval2 e1 env fenv) = 0 then .~(eval2 e2 env fenv)
+    else .~(eval2 e3 env fenv)>.
+
+let rec peval2 p env fenv =
+  match p with
+  | Program ([], e) -> eval2 e env fenv
+  | Program (Declaration (s1, s2, e1) :: tl, e) ->
+    .<let rec f x = .~(eval2 e1 (ext env s2 .<x>.) (ext fenv s1 .<f>.))
+    in .~(peval2 (Program(tl, e)) env (ext fenv s1 .<f>.))>.
+
 let _ =
   let ret = peval1 fact env0 fenv0 in
-  Printf.printf "%d\n" ret
+  Printf.printf "%d\n" ret;
+  let cde = peval2 fact env0 fenv0 in
+  let ret2 = Runnative.run cde in
+  Printf.printf "%d\n" ret2;
+  let invoke = .<fun () -> .~cde>. in
+  let invoke_f = Runnative.run invoke in
+  print_code Format.std_formatter invoke; print_newline();
+  let open Core_bench in
+  [Bench.Test.create ~name:"unstaged" (fun () -> ignore(peval1 fact env0 fenv0));
+   Bench.Test.create ~name:"staged1" (fun () -> ignore(invoke_f ()))]
+  |> Bench.bench
