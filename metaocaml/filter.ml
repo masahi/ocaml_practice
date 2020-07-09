ae28132d351d5d7215d47d0a556aa8abcff764a4
Masahiro Masuda <masahi129@gmail.com>
2020-07-09 11:36:09 +0900

    finish oleg book chap 3

--- metaocaml/filter.ml
+++ metaocaml/filter.ml
@@ -42,4 +42,63 @@ let filter_spine: spine_arr -> (float array -> float array) code =
 
 let _ =
   let cde = .<fun b1 b2 b3 -> .~(filter_spine [| .<b1>.; .<b2>.; .<b3>. |])>. in
-  print_code Format.std_formatter cde; print_newline();
+  print_code Format.std_formatter cde; print_newline()
+
+let filter_staged: float array -> (float array -> float array) code =
+  fun b -> .<fun x ->
+    .~(let m = Array.length b in
+       .<let y i =
+           if i < m - 1 then x.(i) else
+             .~(let sum = ref .<0.0>. in
+                for k = 0 to m - 1 do
+                  let bk = b.(k) in
+                  sum := .<.~(!sum) +. bk *. x.(i-k)>.
+                done;
+               !sum)
+       in
+       Array.init (Array.length x) y
+       >.)
+    >.
+
+let _ =
+  let cde = filter_staged [|0.5; 0.3; 0.2|] in
+  print_code Format.std_formatter cde; print_newline()
+
+let rec convert_list: float code list -> float list code = function
+  | hd :: tl ->
+    let tl_code = convert_list tl in
+    .<.~hd :: .~tl_code>.
+  | [] -> .<[]>.
+
+let convert_array: float code array -> float array code =
+  fun arr ->
+    let lst = List.init (Array.length arr) (fun i -> arr.(i)) in
+    let lst_code = convert_list lst in
+     .<Array.of_list .~lst_code >.
+
+let _ =
+  let cde = convert_array [|.<1.>.; .<2.>.; .<3.>. |] in
+  print_code Format.std_formatter cde; print_newline()
+
+let filter_spine_adaptive ?(threshold=3) :
+  spine_arr -> (float array -> float array) code = fun b ->
+  let m = Array.length b in
+  if m < threshold then
+    filter_spine b
+  else
+    .<let arr_code = .~(convert_array b) in
+    fun x ->
+      let y i =
+        if i < m - 1 then x.(i)
+        else
+          let sum = ref 0.0 in
+          for k = 0 to m - 1 do
+            sum := !sum +. arr_code.(k) *. x.(i - k)
+          done;
+          !sum
+      in
+      Array.init (Array.length x) y>.
+
+let _ =
+  let cde = .<fun b1 b2 b3 b4 -> .~(filter_spine_adaptive [|.<b1>.; .<b2>.; .<b3>. ; .<b4>. |])>. in
+  print_code Format.std_formatter cde; print_newline()
